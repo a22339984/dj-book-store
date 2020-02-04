585146f1935b51544595490f8e399c7dd103ae90
Shih Ting <a22339984@gmail.com>
2020-02-04 21:26:08 +0800

    20200204

--- books/views.py
+++ books/views.py
@@ -1,5 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.viewsets import ModelViewSet
+
+
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from .models import Book
@@ -7,14 +10,21 @@ from .serializers import BookSerializer, BookSerializer2
 
 
 class BookViewSet(ModelViewSet):
-    queryset = Book.objects.all()
+    #queryset = Book.objects.all()
+    queryset = Book.objects.filter(is_online=True)
     serializer_class = BookSerializer
     permission_classes = []
 
+    @action(['get'], False, 'all', 'all')
+    def all_books(self, request):
+        books = Book.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
 
 @api_view(['put'])
 def update_book(request):
     s = BookSerializer2(data=request.data)
     s.is_valid(raise_exception=True)
     Book.objects.filter(pk=1).update(**s.validated_data)
+
     return Response(BookSerializer(Book.objects.first()).data)
